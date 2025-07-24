e9d51cda90514a8c9f5792649a0b7cfd5b041681
LaboratorioInnovacion <psiconervio@gmail.com>
2025-07-24 00:06:40 -0300

    asd

--- modern-shopp/lib/db.js
+++ modern-shopp/lib/db.js
@@ -3,7 +3,8 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = globalThis;
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'error', 'warn']
+  //mostrar logs
+  // log: ['query', 'error', 'warn']
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
