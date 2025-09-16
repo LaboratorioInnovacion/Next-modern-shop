a5faced7b78aba0b7c8ae1de932a209258c640ff
LaboratorioInnovacion <psiconervio@gmail.com>
2025-09-15 21:21:40 -0300

    asda

--- modern-shopp/app/api/products/slug/[slug]/route.js
+++ modern-shopp/app/api/products/slug/[slug]/route.js
@@ -1,5 +1,5 @@
 // app/api/products/slug/[slug]/route.js
-import { prisma } from "../../../../lib/db";
+import { prisma } from "../../../../../../lib/db";
 
 
 export async function GET(req, { params }) {
